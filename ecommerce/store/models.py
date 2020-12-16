71e7e6df74ab44e45561ecfe3cb8ae687487685b
NiniLiao <u9513031@cmu.edu.tw>
2020-12-16 17:57:21 +0800

    modified method ShippingAddress: null=False

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -69,10 +69,10 @@ class OrderItem(models.Model):
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
-    zipcode = models.CharField(max_length=200, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
         
     def __str__(self):
