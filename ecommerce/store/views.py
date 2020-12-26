4849622136bba63be182b8bd6712147e7e84a1d3
NiniLiao <u9513031@cmu.edu.tw>
2020-12-26 19:26:28 +0800

    added register and login

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,14 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.http import JsonResponse
 from .models import *
 import json
 import datetime
 from .utils import cookieCart, cartData, guestOrder
 from .lineapi import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 from pprint import pprint
 
+
 # Create your views here.
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+  
+        context = {'form':form}
+        return render(request, 'accounts/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password =request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
+
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
 
 def store(request):
 
