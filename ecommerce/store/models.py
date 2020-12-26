fca4ef848968e83781228f9a5bc4a91ab3dd71f3
NiniLiao <u9513031@cmu.edu.tw>
2020-12-26 19:25:32 +0800

    added CreateUserForm module

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        exclude = ['user']
 
 class Customer(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name = 'customer', null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self):
         return self.name
