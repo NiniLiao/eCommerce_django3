b345158043b93b5d6d0159c3f73ed2dd3eced5b6
NiniLiao <u9513031@cmu.edu.tw>
2020-12-28 02:01:53 +0800

    fix: add heroku deploy settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -157,3 +158,6 @@ EMAIL_HOST_USER = 'your_account@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+# deploy heroku
+django_heroku.settings(locals())
